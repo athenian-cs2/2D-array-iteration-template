dbec314f569f2ffd75ec5fc0350554a5b6c21f89
sthompson-athenian <sthompson@athenian.org>
2025-08-22 16:01:42 -0700

    Update Gradle to 7.6, Java to 17, and apply code formatting

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,118 +1,107 @@
 public class MyMain {
-    // Returns the String that shows up latest alphabetically
-    // in a normal 1D String array
-    // You can assume that the array will not be empty
-    // and that all Strings are lowercase
-    // Hint: recall how the compareTo() method works:
-    //      int x = "apple".compareTo("banana"); // x is negative
-    //      int y = "banana".compareTo("apple"); // y is positive
-    public static String findLastWord(String[] arr) {
-        // YOUR CODE HERE
-        return "";
-    }
+  // Returns the String that shows up latest alphabetically
+  // in a normal 1D String array
+  // You can assume that the array will not be empty
+  // and that all Strings are lowercase
+  // Hint: recall how the compareTo() method works:
+  //      int x = "apple".compareTo("banana"); // x is negative
+  //      int y = "banana".compareTo("apple"); // y is positive
+  public static String findLastWord(String[] arr) {
+    // YOUR CODE HERE
+    return "";
+  }
 
-    // Given a 2D array, return an 1D array of the last word
-    // in each row in the array
-    // You can assume that the matrix will not be empty
-    // Hint: use the previous method to help yourself!
-    public static String[] findLastWord2D(String[][] mat) {
-        // YOUR CODE HERE
-        return null;
-    }
+  // Given a 2D array, return an 1D array of the last word
+  // in each row in the array
+  // You can assume that the matrix will not be empty
+  // Hint: use the previous method to help yourself!
+  public static String[] findLastWord2D(String[][] mat) {
+    // YOUR CODE HERE
+    return null;
+  }
 
-    // Given a 2D array and some column index col
-    // finds the number of Strings in the specified column
-    // of the 2D array that contain the word "apple"
-    // For example, if col = 0, you should only look through
-    // the values in column 0 of the array; likewise, if
-    // col = 2, you should only look through the values in column 2
+  // Given a 2D array and some column index col
+  // finds the number of Strings in the specified column
+  // of the 2D array that contain the word "apple"
+  // For example, if col = 0, you should only look through
+  // the values in column 0 of the array; likewise, if
+  // col = 2, you should only look through the values in column 2
 
-    // Hint: remember how the indexOf() method works?
-    // alternatively, consider the contains() method
-    public static int appleCounter(String[][] mat, int col) {
-        // YOUR CODE HERE
-        return -1;
-    }
+  // Hint: remember how the indexOf() method works?
+  // alternatively, consider the contains() method
+  public static int appleCounter(String[][] mat, int col) {
+    // YOUR CODE HERE
+    return -1;
+  }
 
-    // Given a 2D array, return the column number corresponding
-    // to the column that contains the most Strings containing
-    // the word "apple"
+  // Given a 2D array, return the column number corresponding
+  // to the column that contains the most Strings containing
+  // the word "apple"
 
-    // Hint: use your previous method!
-    // Hint 2: you might need to loop through the columns!
-    public static int findMostAppleColumn(String[][] mat) {
-        // YOUR CODE HERE
-        return -1;
-    }
+  // Hint: use your previous method!
+  // Hint 2: you might need to loop through the columns!
+  public static int findMostAppleColumn(String[][] mat) {
+    // YOUR CODE HERE
+    return -1;
+  }
 
+  // Creates Pascal's Triangle, with a height of n
+  // The first row of numbers has a single number, 1
+  // Each subsequent row has one more number than the previous row
+  // The first and last numbers of every row are 1
+  // All other numbers’ values are calculated by adding together the two numbers above that number
 
-    // Creates Pascal's Triangle, with a height of n
-    // The first row of numbers has a single number, 1
-    // Each subsequent row has one more number than the previous row
-    // The first and last numbers of every row are 1
-    // All other numbers’ values are calculated by adding together the two numbers above that number
+  // Here are some examples:
+  // pascalTriangle(2) =>
+  // 1  0
+  // 1  1
 
-    // Here are some examples:
-    // pascalTriangle(2) =>
-    // 1  0
-    // 1  1
+  // pascalTriangle(6) =>
+  // 1  0  0  0  0  0
+  // 1  1  0  0  0  0
+  // 1  2  1  0  0  0
+  // 1  3  3  1  0  0
+  // 1  4  6  4  1  0
+  // 1  5  10 10 5  1
 
-    // pascalTriangle(6) =>
-    // 1  0  0  0  0  0
-    // 1  1  0  0  0  0
-    // 1  2  1  0  0  0
-    // 1  3  3  1  0  0
-    // 1  4  6  4  1  0
-    // 1  5  10 10 5  1
+  // Hint: fill in the first column and first diagonal with 1's
+  //       and then go through and calculate the rest of the values
+  //       from top to bottom
 
-    // Hint: fill in the first column and first diagonal with 1's
-    //       and then go through and calculate the rest of the values
-    //       from top to bottom
+  public static int[][] pascal(int height) {
+    // YOUR CODE HERE
+    return null;
+  }
 
-    public static int[][] pascal(int height) {
-        // YOUR CODE HERE
-        return null;
-    }
+  // Methods for the homework:
 
+  // Checks if a 2D array is a magic square or not
+  // You can assume that the 2D array mat will be square
+  // A 2D array is a magic square if:
+  // There is some set value x such that:
+  // * all rows sum to x
+  // * all columns sum to x
+  // * both diagonals sum to x
 
+  // Hint 1: you might first want to add up the values in the
+  // first row/col and save that value to compare with later.
+  // Then, you should check each rows, check each column, and
+  // check each diagonal
 
+  // Hint 2: you problably want to break this down into many parts.
+  // You should have two loops to check the row sums. Then two more
+  // loops to chekc the column sums. Finally, it might help to have
+  // one for loop for each diagonal
 
+  // Hint 3: when thinking the diagonals, consider the following
+  // * do you see any pattern for the row and col indexes for a diagonal?
+  // * can you use a for loop that goes through that pattern?
+  public static boolean isMagic(int[][] mat) {
+    // YOUR CODE HERE
+    return false;
+  }
 
-
-
-
-
-
-    // Methods for the homework:
-
-    // Checks if a 2D array is a magic square or not
-    // You can assume that the 2D array mat will be square
-    // A 2D array is a magic square if:
-    // There is some set value x such that:
-    // * all rows sum to x
-    // * all columns sum to x
-    // * both diagonals sum to x
-
-    // Hint 1: you might first want to add up the values in the
-    // first row/col and save that value to compare with later.
-    // Then, you should check each rows, check each column, and
-    // check each diagonal
-
-    // Hint 2: you problably want to break this down into many parts.
-    // You should have two loops to check the row sums. Then two more
-    // loops to chekc the column sums. Finally, it might help to have
-    // one for loop for each diagonal
-
-    // Hint 3: when thinking the diagonals, consider the following
-    // * do you see any pattern for the row and col indexes for a diagonal?
-    // * can you use a for loop that goes through that pattern?
-    public static boolean isMagic(int[][] mat) {
-        // YOUR CODE HERE
-        return false;
-    }
-
-
-    public static void main(String[] args) {
-        // Write some code here to test your methods!
-    }
+  public static void main(String[] args) {
+    // Write some code here to test your methods!
+  }
 }
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,154 +1,258 @@
-import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
 public class MyTests {
-    @Test
-    public void testFindLastWord() {
-        String[] arr1 = {"apple", "banana", "orange"};
-        String[] arr2 = {"orange", "apple", "banana"};
-        String[] arr3 = {"aardvark", "apple", "acai"};
-        String[] arr4 = {"hello", "world", "computer", "science", "java"};
+  @Test
+  public void testFindLastWord() {
+    String[] arr1 = {"apple", "banana", "orange"};
+    String[] arr2 = {"orange", "apple", "banana"};
+    String[] arr3 = {"aardvark", "apple", "acai"};
+    String[] arr4 = {"hello", "world", "computer", "science", "java"};
 
-        assertEquals("orange", MyMain.findLastWord(arr1),"The last word in " + Arrays.toString(arr1) + " should be: " + "orange");
-        assertEquals("orange", MyMain.findLastWord(arr2),"The last word in " + Arrays.toString(arr2) + " should be: " + "orange");
-        assertEquals("apple", MyMain.findLastWord(arr3),"The last word in " + Arrays.toString(arr3) + " should be: " + "orange");
-        assertEquals("world", MyMain.findLastWord(arr4),"The last word in " + Arrays.toString(arr4) + " should be: " + "world");
-    }
+    assertEquals(
+        "orange",
+        MyMain.findLastWord(arr1),
+        "The last word in " + Arrays.toString(arr1) + " should be: " + "orange");
+    assertEquals(
+        "orange",
+        MyMain.findLastWord(arr2),
+        "The last word in " + Arrays.toString(arr2) + " should be: " + "orange");
+    assertEquals(
+        "apple",
+        MyMain.findLastWord(arr3),
+        "The last word in " + Arrays.toString(arr3) + " should be: " + "orange");
+    assertEquals(
+        "world",
+        MyMain.findLastWord(arr4),
+        "The last word in " + Arrays.toString(arr4) + " should be: " + "world");
+  }
 
-    @Test
-    public void testFindLastWord2D() {
-        String[][] mat1 = {  {"apple", "banana", "orange"},
-                             {"kiwi", "grapefruit", "fig"},
-                             {"lemon", "lime", "pomelo"}
-        };
+  @Test
+  public void testFindLastWord2D() {
+    String[][] mat1 = {
+      {"apple", "banana", "orange"},
+      {"kiwi", "grapefruit", "fig"},
+      {"lemon", "lime", "pomelo"}
+    };
 
-        String[] arr1 = {"orange", "kiwi", "pomelo"};
+    String[] arr1 = {"orange", "kiwi", "pomelo"};
 
-        assertEquals(Arrays.toString(arr1), Arrays.toString(MyMain.findLastWord2D(mat1)),"The last words in \n" + Arrays.deepToString(mat1) + "\nshould be: " + Arrays.toString(arr1));
-    }
+    assertEquals(
+        Arrays.toString(arr1),
+        Arrays.toString(MyMain.findLastWord2D(mat1)),
+        "The last words in \n"
+            + Arrays.deepToString(mat1)
+            + "\nshould be: "
+            + Arrays.toString(arr1));
+  }
 
-    @Test
-    public void testAppleCounter() {
-        String[][] mat1 = {  {"apple", "banana", "fig"},
-                             {"kiwi", "pineapple", "applesauce"},
-                             {"crabapple", "lime", "pomelo"}
-        };
+  @Test
+  public void testAppleCounter() {
+    String[][] mat1 = {
+      {"apple", "banana", "fig"},
+      {"kiwi", "pineapple", "applesauce"},
+      {"crabapple", "lime", "pomelo"}
+    };
 
-        assertEquals(2, MyMain.appleCounter(mat1, 0),"The number of apple words in col 0 of \n" + Arrays.deepToString(mat1) + "\nshould be: " + 2);
-        assertEquals(1, MyMain.appleCounter(mat1, 1),"The number of apple words in col 1 of \n" + Arrays.deepToString(mat1) + "\nshould be: " + 1);
-        assertEquals(1, MyMain.appleCounter(mat1, 2),"The number of apple words in col 2 of \n" + Arrays.deepToString(mat1) + "\nshould be: " + 1);
-    }
+    assertEquals(
+        2,
+        MyMain.appleCounter(mat1, 0),
+        "The number of apple words in col 0 of \n"
+            + Arrays.deepToString(mat1)
+            + "\nshould be: "
+            + 2);
+    assertEquals(
+        1,
+        MyMain.appleCounter(mat1, 1),
+        "The number of apple words in col 1 of \n"
+            + Arrays.deepToString(mat1)
+            + "\nshould be: "
+            + 1);
+    assertEquals(
+        1,
+        MyMain.appleCounter(mat1, 2),
+        "The number of apple words in col 2 of \n"
+            + Arrays.deepToString(mat1)
+            + "\nshould be: "
+            + 1);
+  }
 
-    @Test
-    public void findMostAppleColumn() {
-        String[][] mat1 = {  {"apple", "banana", "fig"},
-                {"kiwi", "pineapple", "applesauce"},
-                {"crabapple", "lime", "pomelo"}
-        };
+  @Test
+  public void findMostAppleColumn() {
+    String[][] mat1 = {
+      {"apple", "banana", "fig"},
+      {"kiwi", "pineapple", "applesauce"},
+      {"crabapple", "lime", "pomelo"}
+    };
 
-        String[][] mat2 = {  {"apple", "banana", "fig"},
-                {"kiwi", "pineapple", "applesauce"},
-                {"crabapple", "lime", "pomelo"},
-                {"orange", "lime", "apples"},
-                {"orange", "lime", "twoapples"}
-        };
+    String[][] mat2 = {
+      {"apple", "banana", "fig"},
+      {"kiwi", "pineapple", "applesauce"},
+      {"crabapple", "lime", "pomelo"},
+      {"orange", "lime", "apples"},
+      {"orange", "lime", "twoapples"}
+    };
 
-        assertEquals(0, MyMain.findMostAppleColumn(mat1),"The column with the most apple words in \n" + Arrays.deepToString(mat1) + "\nshould be: " + 0);
-        assertEquals(2, MyMain.findMostAppleColumn(mat2),"The column with the most apple words in \n" + Arrays.deepToString(mat2) + "\nshould be: " + 2);
-    }
+    assertEquals(
+        0,
+        MyMain.findMostAppleColumn(mat1),
+        "The column with the most apple words in \n"
+            + Arrays.deepToString(mat1)
+            + "\nshould be: "
+            + 0);
+    assertEquals(
+        2,
+        MyMain.findMostAppleColumn(mat2),
+        "The column with the most apple words in \n"
+            + Arrays.deepToString(mat2)
+            + "\nshould be: "
+            + 2);
+  }
 
-    @Test
-    public void testPascal() {
-        // Here are some examples:
-        // pascalTriangle(2) =>
-        // 1  0
-        // 1  1
+  @Test
+  public void testPascal() {
+    // Here are some examples:
+    // pascalTriangle(2) =>
+    // 1  0
+    // 1  1
 
-        // pascalTriangle(6) =>
-        // 1  0  0  0  0  0
-        // 1  1  0  0  0  0
-        // 1  2  1  0  0  0
-        // 1  3  3  1  0  0
-        // 1  4  6  4  1  0
-        // 1  5  10 10 5  1
+    // pascalTriangle(6) =>
+    // 1  0  0  0  0  0
+    // 1  1  0  0  0  0
+    // 1  2  1  0  0  0
+    // 1  3  3  1  0  0
+    // 1  4  6  4  1  0
+    // 1  5  10 10 5  1
 
-        int[][] m1 = {
-                {1, 0},
-                {1, 1}};
+    int[][] m1 = {
+      {1, 0},
+      {1, 1}
+    };
 
-        int[][] m2 = {
-                {1, 0, 0, 0, 0, 0},
-                {1, 1, 0, 0, 0, 0},
-                {1, 2, 1, 0, 0, 0},
-                {1, 3, 3, 1, 0, 0},
-                {1, 4, 6, 4, 1, 0},
-                {1, 5, 10, 10, 5, 1}};
+    int[][] m2 = {
+      {1, 0, 0, 0, 0, 0},
+      {1, 1, 0, 0, 0, 0},
+      {1, 2, 1, 0, 0, 0},
+      {1, 3, 3, 1, 0, 0},
+      {1, 4, 6, 4, 1, 0},
+      {1, 5, 10, 10, 5, 1}
+    };
 
-        assertEquals(Arrays.deepToString(m1), Arrays.deepToString(MyMain.pascal(2)),"A pascal triangle of height 2 should look like" + Arrays.deepToString(m1));
-        assertEquals(Arrays.deepToString(m2), Arrays.deepToString(MyMain.pascal(6)),"A pascal triangle of height 6 should look like" + Arrays.deepToString(m2));
-    }
+    assertEquals(
+        Arrays.deepToString(m1),
+        Arrays.deepToString(MyMain.pascal(2)),
+        "A pascal triangle of height 2 should look like" + Arrays.deepToString(m1));
+    assertEquals(
+        Arrays.deepToString(m2),
+        Arrays.deepToString(MyMain.pascal(6)),
+        "A pascal triangle of height 6 should look like" + Arrays.deepToString(m2));
+  }
 
+  @Test
+  public void testIsMagic() {
+    int[][] m1 = {
+      {4, 9, 2},
+      {3, 5, 7},
+      {8, 1, 6}
+    };
 
-    @Test
-    public void testIsMagic() {
-        int[][] m1 = {
-                {4, 9, 2},
-                {3, 5, 7},
-                {8, 1, 6}};
+    int[][] m2 = {
+      {2, 16, 13, 3},
+      {11, 5, 8, 10},
+      {7, 9, 12, 6},
+      {14, 4, 1, 15}
+    };
 
+    int[][] m3 = {
+      {1, 23, 16, 4, 21},
+      {15, 14, 7, 18, 11},
+      {24, 17, 13, 9, 2},
+      {20, 8, 19, 12, 6},
+      {5, 3, 10, 22, 25}
+    };
 
-        int[][] m2 = {
-                { 2, 16, 13,  3},
-                {11,  5,  8, 10},
-                { 7,  9, 12,  6},
-                {14,  4,  1, 15}};
+    // rows all same total, but not cols
+    int[][] m4 = {
+      {100, 101, 99, 100},
+      {98, 99, 100, 103},
+      {106, 94, 103, 97},
+      {100, 100, 100, 100}
+    };
 
-        int[][] m3 = {
-                { 1, 23, 16,  4, 21},
-                {15, 14,  7, 18, 11},
-                {24, 17, 13,  9,  2},
-                {20,  8, 19, 12,  6},
-                { 5,  3, 10, 22, 25}};
+    // rows and cols all same total, but not diagonals
+    int[][] m5 = {
+      {1, 2, 3, 4},
+      {2, 3, 4, 1},
+      {3, 4, 1, 2},
+      {4, 1, 2, 3}
+    };
 
-        // rows all same total, but not cols
-        int[][] m4 = {
-                {100, 101, 99, 100},
-                {98,  99,  100, 103},
-                {106, 94, 103, 97},
-                {100, 100, 100, 100}
-        };
+    int[][] m6 = {
+      {42, 37, 23, -5, 9},
+      {11, 6, 22, 12, 13},
+      {14, 5, 17, 13, 20},
+      {1, 4, 27, 7, 32},
+      {6, 2, 38, 1, 44}
+    };
 
-        // rows and cols all same total, but not diagonals
-        int[][] m5 = {
-                {1, 2, 3, 4},
-                {2, 3, 4, 1},
-                {3, 4, 1, 2},
-                {4, 1, 2, 3}
-        };
+    int[][] m7 = {
+      {37, 23, -5, 9, 42},
+      {6, 22, 12, 13, 11},
+      {5, 17, 13, 20, 14},
+      {4, 27, 7, 32, 1},
+      {2, 38, 1, 44, 6}
+    };
 
-        int[][] m6 = {
-                {42, 37, 23, -5,  9},
-                {11,  6, 22, 12, 13},
-                {14,  5, 17, 13, 20},
-                {1,   4, 27,  7, 32},
-                {6,  2,  38,  1, 44} };
-
-        int[][] m7 = {
-                {37, 23, -5,  9, 42},
-                { 6, 22, 12, 13, 11},
-                { 5, 17, 13, 20, 14},
-                { 4, 27,  7, 32,  1},
-                {2,  38,  1, 44,  6} };
-
-        assertEquals(true, MyMain.isMagic(m1),"Testing whether the array:\n" + Arrays.deepToString(m1) + "\n is magic should be: " + true);
-        assertEquals(true, MyMain.isMagic(m2),"Testing whether the array:\n" + Arrays.deepToString(m2) + "\n is magic should be: " + true);
-        assertEquals(true, MyMain.isMagic(m3),"Testing whether the array:\n" + Arrays.deepToString(m3) + "\n is magic should be: " + true);
-        assertEquals(false, MyMain.isMagic(m6),"Testing whether the array:\n" + Arrays.deepToString(m6) + "\n is magic should be: " + false);
-        assertEquals(false, MyMain.isMagic(m7),"Testing whether the array:\n" + Arrays.deepToString(m7) + "\n is magic should be: " + false);
-        assertEquals(false, MyMain.isMagic(m4),"Testing whether the array:\n" + Arrays.deepToString(m4) + "\n is magic should be: " + false);
-        assertEquals(false, MyMain.isMagic(m5),"Testing whether the array:\n" + Arrays.deepToString(m5) + "\n is magic should be: " + false);
-    }
+    assertEquals(
+        true,
+        MyMain.isMagic(m1),
+        "Testing whether the array:\n"
+            + Arrays.deepToString(m1)
+            + "\n is magic should be: "
+            + true);
+    assertEquals(
+        true,
+        MyMain.isMagic(m2),
+        "Testing whether the array:\n"
+            + Arrays.deepToString(m2)
+            + "\n is magic should be: "
+            + true);
+    assertEquals(
+        true,
+        MyMain.isMagic(m3),
+        "Testing whether the array:\n"
+            + Arrays.deepToString(m3)
+            + "\n is magic should be: "
+            + true);
+    assertEquals(
+        false,
+        MyMain.isMagic(m6),
+        "Testing whether the array:\n"
+            + Arrays.deepToString(m6)
+            + "\n is magic should be: "
+            + false);
+    assertEquals(
+        false,
+        MyMain.isMagic(m7),
+        "Testing whether the array:\n"
+            + Arrays.deepToString(m7)
+            + "\n is magic should be: "
+            + false);
+    assertEquals(
+        false,
+        MyMain.isMagic(m4),
+        "Testing whether the array:\n"
+            + Arrays.deepToString(m4)
+            + "\n is magic should be: "
+            + false);
+    assertEquals(
+        false,
+        MyMain.isMagic(m5),
+        "Testing whether the array:\n"
+            + Arrays.deepToString(m5)
+            + "\n is magic should be: "
+            + false);
+  }
 }
